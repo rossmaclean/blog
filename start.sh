7eb66c402ea37aef48aabba98aca5fe515703082
Ross MacLean <ross@rossmac.co.uk>
2022-07-19 11:37:45 +0100

    Add no backup.

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 echo "=> Update nginx.conf"
 sed -e "s,##APP_DOMAIN##,${CLOUDRON_APP_DOMAIN}," /app/code/nginx.conf  > /run/nginx.conf
 
-echo "Starting Nginx"``
+echo "Starting Nginx"
 nginx -c /run/nginx.conf
 #exec /usr/local/bin/gosu cloudron:cloudron nginx -g 'daemon off;'
